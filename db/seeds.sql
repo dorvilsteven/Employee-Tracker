f24b6346808f7636e6b4ae98bebc1aadd8891895
StevenD <dorvilsteven@gmail.com>
2021-12-01 17:07:04 -0500

    database schema

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO departments (name) 
+VALUES
+('Corporate'),
+('Finance'),
+('Sales'),
+('Engineering');
+
+INSERT INTO roles (title, salary, deparment_id) 
+VALUES 
+('Sales Person', 50000, 3),
+('Software Engineer', 100000, 4),
+('Lead Engineer', 150000, 4),
+('Accountant', 80000, 2);
+
